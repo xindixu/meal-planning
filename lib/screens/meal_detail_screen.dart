3cb701501d5040616be04176f174a1f2747cd930
Xindi Xu <xindixu@utexas.edu>
2019-07-13 23:59:14 -0500

    finish meal detail page

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -12,14 +12,68 @@ class MealDetailScreen extends StatelessWidget {
 
     final id = routeArg['id'];
 
-    final Meal meal = DUMMY_MEALS.where((meal) => meal.id == id).toList()[0];
+    final Meal meal = DUMMY_MEALS.firstWhere((meal) => meal.id == id);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('${meal.title}'),
       ),
-      body: Center(
-        child: Text('hah'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Ingredients',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Theme.of(context).accentColor),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              height: 200,
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.all(20),
+              child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(
+                  color: Theme.of(context).accentColor,
+                ),
+                itemBuilder: (context, index) => Text(meal.ingredients[index]),
+                itemCount: meal.ingredients.length,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Steps',
+                style: Theme.of(context).textTheme.title,
+              ),
+            ),
+            Container(
+              height: 800,
+              child: ListView.builder(
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    child: Text('${index + 1}'),
+                  ),
+                  title: Text(meal.steps[index]),
+                ),
+                itemCount: meal.steps.length,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
