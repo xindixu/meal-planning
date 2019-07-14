b53706990af6b9eae9a3783e89ff4c46108a1ff7
Xindi Xu <xindixu@utexas.edu>
2019-07-13 21:15:01 -0500

    add category screen

---
+++ lib/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import './category_item.dart';
+import './dummy-data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meal Planning'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (category) => CategoryItem(category.title, category.color),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
